b820122336ecbe29ba22d6e68e859294649e35db
Supertiger <saawalkhn@googlemail.com>
2025-03-26 18:47:21 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import HeroesPage from './pages/HeroesPage'
+
+function App() {
+
+  return (
+    <>
+      <HeroesPage/>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/HeroesPage.tsx
@@ -0,0 +1,10 @@
+import style from './HeroesPage.module.scss';
+const HeroesPage = () => {
+    return (
+        <div class={style.container}>
+            <h1>Heroes</h1>
+        </div>
+    );
+};
+
+export default HeroesPage;
