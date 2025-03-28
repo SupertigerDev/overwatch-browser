d37eb0398ab432d9216260dc30e4759cefdf175e
SupertigerDev <SupertigerDev@users.noreply.github.com>
2025-03-28 09:08:33 +0000

    deploy: 53e787d6fca5f6081d7734d27fa1eeb815e92a3e

---
+++ assets/dva-DEfuDBHT.js
@@ -0,0 +1,1 @@
+const e=[{id:"overwatch-1",tier:"common",name:"Overwatch 1"},{id:"overwatch-2",tier:"common",name:"Overwatch 2"},{id:"academy",tier:"legendary",name:"Academy",credits:1500},{id:"cruiser",tier:"legendary",name:"Cruiser",credits:1500},{id:"varsity",tier:"legendary",name:"Varsity",credits:1500}];export{e as skins};
